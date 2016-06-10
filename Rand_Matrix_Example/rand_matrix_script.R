5d4fc68197ba8a487d1bc67d74e4f5a7adcf0fe2
Alex <alex.simmons.1992@gmail.com>
2016-06-10 19:31:35 +1000

    Updated for the new readme.md

--- Rand_Matrix_Example/rand_matrix_script.R
+++ Rand_Matrix_Example/rand_matrix_script.R
@@ -2,22 +2,14 @@ args<-commandArgs(TRUE)
 
 # Set some defaults
 seed <- 1
-n <- 10
-m <- 10
+rows <- 10
+cols <- 10
 
 # Replace defaults with arguments if they exist
-nargs = length(args)
-if (nargs >= 1) {
-  seed <- eval( parse(text=args[1]))
-  if (nargs >= 2) {
-    n <- eval( parse(text=args[2]))
-    if (nargs >= 3) {
-      m <- eval( parse(text=args[3]))
-    }
-  }
+for(i in 1:length(args)){
+  eval(parse(text=args[[i]]))
 }
+
 set.seed(seed)
-
-
-print(c(seed, n, m))
-print(matrix(rexp(200, rate=.1),nrow=n,ncol=m))
+print(c(seed, rows, cols))
+print(matrix(rexp(200, rate=.1),rows,cols))
